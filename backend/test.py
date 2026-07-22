dc1287ccb1828e296deca086ea3cfed4a11d1af6
Debian <debian@debian13.linuxvmimages.local>
2026-07-22 07:32:24 -0500

    added sql injection for semgrep to detect

--- backend/test.py
+++ backend/test.py
@@ -1,3 +1,12 @@
-name = input ("hie")
-result = eval(name)
-print(result)
+import sqlite3
+
+conn = sqlite3.connect("users.db")
+cursor = conn.cursor()
+
+username = input("Enter username: ")
+
+query = "SELECT * FROM users WHERE username = '" + username + "'"
+
+cursor.execute(query)
+
+print(cursor.fetchall())
